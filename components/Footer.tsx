a8e6541f168fbe68370ef59bb163a96d3c578e10
VinceRamirez1998 <it.benedictramirez247@gmail.com>
2025-06-18 19:07:35 +0800

    Footer Socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,42 @@
 // components/Footer.tsx
-import { Image, StyleSheet } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  Linking,
+  Pressable,
+  StyleSheet,
+  View,
+} from "react-native";
 
 export default function Footer() {
+  const socials = [
+    {
+      icon: require("@/assets/icons/internet.png"),
+      url: "https://gothamautoworks.com",
+    },
+    {
+      icon: require("@/assets/icons/facebook.png"),
+      url: "https://facebook.com/gothamautoworks",
+    },
+    {
+      icon: require("@/assets/icons/instagram.png"),
+      url: "https://instagram.com/gothamautoworks",
+    },
+  ];
+
   return (
-    <Image
-      source={require("@/assets/images/background-car.png")}
+    <ImageBackground
+      source={require("@/assets/images/footer.png")}
       style={styles.footerImage}
-    />
+    >
+      <View style={styles.socialContainer}>
+        {socials.map((item, index) => (
+          <Pressable key={index} onPress={() => Linking.openURL(item.url)}>
+            <Image source={item.icon} style={styles.icon} />
+          </Pressable>
+        ))}
+      </View>
+    </ImageBackground>
   );
 }
 
@@ -14,7 +44,17 @@ const styles = StyleSheet.create({
   footerImage: {
     width: "100%",
     height: 264,
-    marginTop: 0,
-    borderRadius: 0,
+    justifyContent: "flex-end", // position icons at the bottom
+    padding: 16,
+  },
+  socialContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: 16,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    tintColor: "#fff", // optional: applies a white overlay
   },
 });

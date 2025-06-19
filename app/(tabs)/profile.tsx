da6b7a116bb27f037088aff072a1447f9cb02b6a
VinceRamirez1998 <it.benedictramirez247@gmail.com>
2025-06-19 14:34:44 +0800

    tabs

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import Footer from "@/components/Footer"; // adjust the path if needed
+import { StyleSheet, View } from "react-native";
 
 export default function ProfileScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <Footer />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000", // to match your app’s dark theme
+    // justifyContent: "flex-end",
+    justifyContent: "center",
+  },
+});
